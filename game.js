a4d265d557dd155398f5c63d38720d324040c7f4
Saturn <138524367+earth2saturn@users.noreply.github.com>
2024-05-18 21:46:42 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const storyElement = document.getElementById('story');
     const choicesElement = document.getElementById('choices');
 
